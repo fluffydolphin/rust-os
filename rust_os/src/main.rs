bd3b29e3f843e3843f90e3ab31d7e9f046ef496d
fluffydolphin <fluffy@fluffydolphin.xyz>
2023-06-28 03:20:43 +1200

    update

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -6,20 +6,13 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{_info}");
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", some numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
+    println!("Hello world!");
 
     loop {}
 }
